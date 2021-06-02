ac361ddbb8df7a7ca88609421a16f1e98bb7a2f8
PedroL <pedrolucassantosferreira426@gmail.com>
2021-06-01 23:08:19 -0300

    .

--- src/app/shared/shared.functions.ts
+++ src/app/shared/shared.functions.ts
@@ -49,12 +49,12 @@ export class SharedFunctions {
     this.meta.updateTag({name: 'twitter:site', content: '@content'});
     this.meta.updateTag({name: 'twitter:title', content: title});
     this.meta.updateTag({name: 'twitter:description', content: 'teste'});
-    // this.meta.updateTag({name: 'twitter:image', content: config.image});
+    this.meta.updateTag({name: 'twitter:image', content: 'https://veja.abril.com.br/wp-content/uploads/2019/12/amazonia-floresta-coraccca7ao.jpg.jpg'});
     this.meta.updateTag({property: 'og:type', content: 'article'});
     this.meta.updateTag({property: 'og:site_name', content: 'content'});
     this.meta.updateTag({property: 'og:title', content: title});
     this.meta.updateTag({property: 'og:description', content: 'descricao top'});
-    // this.meta.updateTag({property: 'og:image', content: config.image});
+    this.meta.updateTag({property: 'og:image', content: 'https://veja.abril.com.br/wp-content/uploads/2019/12/amazonia-floresta-coraccca7ao.jpg.jpg'});
     // this.meta.updateTag({property: 'og:url', content: `https://www.example.com/${config.slug}`});
   }
 }
